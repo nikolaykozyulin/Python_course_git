2a32b7b0aa092aaf998e6d0018ec03954db03f5c
Nikolay_Kozyulin <nikkozyulin@gmail.com>
2023-09-05 21:48:51 +0300

    Task 2_6 commit

---
+++ lesson2/hw_task_2_6.py
@@ -0,0 +1,40 @@
+
+
+def int_decorator(f):
+    def result(x):
+        try:
+            r = f(x)
+            type(r) == int
+        except Exception as ex:
+            print("Exception call")
+            print(ex)
+    return result
+
+
+@int_decorator
+def square_list(list_values):
+    #list_values ** 2
+    return print(list(map(lambda x: x ** 2, list_values)))
+
+
+@int_decorator
+def print_list(word_list):
+    print(word_list)
+
+
+@int_decorator
+def square_number(number):
+    return print(number ** 2)
+
+
+
+#def no_space(word_list):
+#    return print(word_list)
+
+
+list_values = [1, 2, 3, 4]
+word_list = "Monday Evning"
+square_list(list_values)
+square_number(10)
+print_list(word_list)
+
