aef3127e08213ae7e29bf1fb1bc486358cfc667c
Nikolay_Kozyulin <nikkozyulin@gmail.com>
2023-09-05 21:13:06 +0300

    Task 2_1 commit corrected

--- lesson2/hw_task_2_1.py
+++ lesson2/hw_task_2_1.py
@@ -4,7 +4,12 @@ def stopWordsControl(list_value):
     print(list(filter(lambda x: x in stop_list, list_value)))
 
 
+
+
 stop_list = ['Python', 'php', 'go', 'C']
-list_value = ['Python', 'go', 'to', 'University']
+string_value = 'Python go to University'
+list_value = string_value.split(" ")
+print(list_value)
+
 stopWordsControl(list_value)
 
